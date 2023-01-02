78d5accec38f01bc818ed055b97fd5726693b7e2
spacecat <72170757+imaspacecat@users.noreply.github.com>
2023-01-01 16:18:12 -0800

    added docs

--- core/src/main/kotlin/com/asiankoala/koawalib/control/filter/SlewRateLimiter.kt
+++ core/src/main/kotlin/com/asiankoala/koawalib/control/filter/SlewRateLimiter.kt
@@ -8,14 +8,25 @@ class SlewRateLimiter(
 ) {
     private var ukm1 = 0.0
     private var tkm1 = Clock.seconds
-
+    
+    /**
+     * Filters the input to limit its slew rate.
+     *
+     * @param input The input value whose slew rate is to be limited.
+     * @return The filtered value, which will not change faster than the slew rate.
+     */
     fun calculate(input: Double): Double {
         val dt = Clock.seconds - tkm1
         ukm1 += clamp(input - ukm1, -r * dt, r * dt)
         tkm1 = Clock.seconds
         return ukm1
     }
-
+    
+    /**
+     * Resets the slew rate limiter to the specified value; ignores the rate limit when doing so.
+     *
+     * @param u The value to reset to.
+     */
     fun reset(u: Double) {
         ukm1 = u
         tkm1 = Clock.seconds
